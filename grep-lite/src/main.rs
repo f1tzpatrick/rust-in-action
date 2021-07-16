488a58185d3717bf553188ba617556178d08030d
Steven Fitzpatrick <steven@fitz.gg>
2021-07-16 05:51:16 +0000

    Let grep lite read from stdin

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -1,27 +1,24 @@
 // grep very-lite
 
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
-use std::io::SeekFrom;
 use regex::Regex;
 use clap::{App, Arg};
 
 fn main() {
-
     let args = App::new("grep-lite")
         .version("0.1.0")
         .about("searches for terms in text")
         .arg(Arg::with_name("search-term")
             .help("A String pattern to search for")
-            .long("search-term")
             .takes_value(true)
             .required(true))
-        .arg(Arg::with_name("input")
-            .help("Text to search through")
+        .arg(Arg::with_name("source-text")
+            .help("Path to a text file to search. Reads from stdin if not provided")
             .long("input")
-            .takes_value(true)
-            .required(true))
+            .takes_value(true))
         .arg(Arg::with_name("context-size")
             .help("The number of lines to display above and below a match")
             .short("C")
@@ -33,9 +30,9 @@ fn main() {
         None => panic!("You need to provide a search term")
     };
 
-    let input = match args.value_of("input") {
+    let source_text = match args.value_of("source-text") {
         Some(input) => input,
-        None => panic!("You need to provide some text to search")
+        None => "",
     };
 
     let context_lines = match args.value_of("context-size") {
@@ -46,7 +43,7 @@ fn main() {
         None => 0,
     };
 
-    println!("{}", grep(input, search_term, context_lines));
+    println!("{}", grep(source_text, search_term, context_lines));
 }
 
 fn grep<'a>(
@@ -55,35 +52,42 @@ fn grep<'a>(
     context_size: usize
     ) -> String {
     
-    let re = Regex::new(search_term).unwrap();
+    // Shadow source_text to a String read from stdin or the file pased as input
+    let source_text = match File::open(source_text) {
+        Ok(file) =>  {
+            let mut reader = BufReader::new(file);
+            let mut lines = String::new();
+            reader.read_to_string(&mut lines).unwrap();
+            lines
+        },
+        Err(_) => {
+            let stdin = io::stdin();
+            let mut reader = stdin.lock();
+            let mut lines = String::new();
+            reader.read_to_string(&mut lines).unwrap();
+            lines
+        }
+    };
+
+    let search_term = Regex::new(search_term).unwrap();
+
     let mut matching_lines: Vec<usize> = Vec::new();
     let mut output = String::new();
-    
-    // Create a mutable BufReader and iterate over it by reference, to support
-    // reuse of the underlying File object
-    let source_text = File::open(source_text).unwrap();
-    let mut reader = BufReader::new(source_text);
 
     // First loop: Identify matching lines
-    for (line_num, text) in reader.by_ref().lines().enumerate() {
-        let text = text.unwrap();
-        match re.find(&text) {
+    for (line_num, text) in source_text.lines().enumerate() {
+        match search_term.find(text) {
             Some(_) => matching_lines.push(line_num),
             None => (),
         }
     }
 
     if matching_lines.is_empty() {
-        return "".to_string()
+        return output
     }
 
-    // At this point the reader has read through to the end of the file
-    // In order to read it again, we must move the cursor back to the start
-    reader.seek(SeekFrom::Start(0)).unwrap();
-
     // Second Loop: Grab content from source_text for output
-    for (line_num, text) in reader.by_ref().lines().enumerate() {
-        let text = text.unwrap();
+    for (line_num, text) in source_text.lines().enumerate() {
         for target in matching_lines.iter() {
             
             let lower_bound = target.saturating_sub(context_size);
